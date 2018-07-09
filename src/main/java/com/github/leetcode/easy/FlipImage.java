cd81251e28b169097135ef359b955ffaf40112ba
SlamDunk <1430701240@qq.com>
2018-07-09 23:20:24 +0800

    solve 4 questions on leet-code

---
+++ src/main/java/com/github/leetcode/easy/FlipImage.java
@@ -0,0 +1,65 @@
+package com.github.leetcode.easy;
+
+/**
+ * Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
+ * <p>
+ * To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+ * <p>
+ * To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [[1,1,0],[1,0,1],[0,0,0]]
+ * Output: [[1,0,0],[0,1,0],[1,1,1]]
+ * Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+ * Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+ */
+public class FlipImage {
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] flipAndInvertImage(int[][] A) {
+        int[][] flipImage = flipImage(A);
+        return invertImage(flipImage);
+    }
+
+    /**
+     * flip an image
+     *
+     * @param originalImage
+     * @return
+     */
+    public int[][] flipImage(int[][] originalImage) {
+        int temp;
+        for (int i = 0; i < originalImage.length; i++) {
+            for (int j = 0, k = originalImage[i].length - 1; j < originalImage[i].length / 2; j++, k--) {
+                temp = originalImage[i][k];
+                originalImage[i][k] = originalImage[i][j];
+                originalImage[i][j] = temp;
+            }
+        }
+        return originalImage;
+    }
+
+    /**
+     * invert an image
+     *
+     * @param originalImage
+     * @return
+     */
+    public int[][] invertImage(int[][] originalImage) {
+        for (int i = 0; i < originalImage.length; i++) {
+            for (int j = 0; j < originalImage[i].length; j++) {
+                if (originalImage[i][j] == 0) {
+                    originalImage[i][j] = 1;
+                } else {
+                    originalImage[i][j] = 0;
+                }
+
+            }
+        }
+        return originalImage;
+    }
+}
+
---
+++ src/main/java/com/github/leetcode/easy/TransposeMatrix.java
@@ -0,0 +1,47 @@
+package com.github.leetcode.easy;
+
+/**
+ * Given a matrix A, return the transpose of A.
+ * <p>
+ * The transpose of a matrix is the matrix flipped over it's main diagonal, switching the row and column indices of the matrix.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [[1,2,3],[4,5,6],[7,8,9]]
+ * Output: [[1,4,7],[2,5,8],[3,6,9]]
+ * Example 2:
+ * <p>
+ * Input: [[1,2,3],[4,5,6]]
+ * Output: [[1,4],[2,5],[3,6]]
+ */
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        int[][] sourceMatrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] newMatrix = transpose(sourceMatrix);
+        System.out.println(newMatrix);
+    }
+
+    /**
+     * transpose a matrix
+     *
+     * @param A
+     * @return
+     */
+    public static int[][] transpose(int[][] A) {
+        if (A != null && A.length > 0) {
+            int rowLength = A.length;
+            int columnLength = A[0].length;
+            int[][] newMatrix = new int[A[0].length][A.length];
+            for (int i = 0; i < rowLength; i++) {
+                for (int j = 0; j < columnLength; j++) {
+                    newMatrix[j][i] = A[i][j];
+                }
+            }
+            return newMatrix;
+        } else {
+            return null;
+        }
+    }
+}
