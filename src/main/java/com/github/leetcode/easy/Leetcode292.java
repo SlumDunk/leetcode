fc7c173cafdbc29245b7e1b63e8af1228ff37f79
SlamDunk <1430701240@qq.com>
2019-01-03 11:40:20 -0500

    practice for brainteaser topic

---
+++ src/main/java/com/github/leetcode/easy/Leetcode292.java
@@ -0,0 +1,23 @@
+package com.github.leetcode.easy;
+
+/**
+ * @Author: zerongliu
+ * @Date: 1/3/19 11:22
+ * @Description: You are playing the following Nim Game with your friend: There is a heap of stones on the table, each time one of you take turns to remove 1 to 3 stones. The one who removes the last stone will be the winner. You will take the first turn to remove the stones.
+ * <p>
+ * Both of you are very clever and have optimal strategies for the game. Write a function to determine whether you can win the game given the number of stones in the heap.
+ * <p>
+ * Example:
+ * <p>
+ * Input: 4
+ * Output: false
+ * Explanation: If there are 4 stones in the heap, then you will never win the game;
+ * No matter 1, 2, or 3 stones you remove, the last stone will always be
+ * removed by your friend.
+ */
+public class Leetcode292 {
+    public boolean canWinNim(int n) {
+        //只要对4求余不等于0就能获胜
+        return n % 4 != 0;
+    }
+}
